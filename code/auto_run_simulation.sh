48ae05a8b254912133ec9ee0200995e11ef66ca2
Blawker <thomas.g7685@gmail.com>
2020-07-07 12:47:53 +0200

    Modify config files, add output merge

--- code/auto_run_simulation.sh
+++ code/auto_run_simulation.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # Must be executed in UCN-Simulations/code/
-seed="8131"
-output_file="beamline_surface_auto_simulation_triga_3"
+SECONDS=0
+seed="813"
+output_file="beamline_surface_auto_simulation_triga_"
 ext=".root"
 cd ../../../Kassiopeia/Kasper_build
 pwd
@@ -9,9 +10,25 @@ read -p 'Run the simulation ? [Y / N] ' -n 1 isNewRun
 echo -e "\n"
 if [ $isNewRun == "Y" ]
 then
-  for i in `seq 1 4`;
+  for i in `seq 0 6`;
   do
     clear
-    ./bin/Kassiopeia.app/Contents/MacOS/Kassiopeia ./../../Stage\ 2A\ -\ UCN\ -\ 2020/UCN-Simulations/config/Beamline/beamline_surface_auto.xml -r random_seed=$seed$i output_file_name=$output_file$i$ext
+    for j in `seq 0 9`;
+    do
+      clear
+      ./bin/Kassiopeia.app/Contents/MacOS/Kassiopeia ../../Stage\ 2A\ -\ UCN\ -\ 2020/UCN-Simulations/config/Beamline/beamline_surface_auto.xml -r random_seed=$seed$i$j output_file_name=$output_file$i$j$ext &
+    done
+    wait
   done
+  duration=$SECONDS
+
+  # Merge of the output files
+  cd ../../Stage\ 2A\ -\ UCN\ -\ 2020/UCN-Simulations/output/Beamline/auto_parallel/
+  hadd -f ../merge/beamline_surface_TRIGA_merge_parallel.root `ls`
+
+  # Calcul the time of the simulation
+  echo ""
+  echo "Time of the simulation: $(($duration / 3600))h $(($duration / 60))min $(($duration % 60))s."
+  echo "Stop: `date`"
+  #shutdown now
 fi
--- code/merge.sh
+++ code/merge.sh
@@ -1,42 +1,5 @@
 #!/bin/bash
 # Must be executed in UCN-Simulations/code/
-path_merge_file=../output/Beamline/merge/beamline_surface_TRIGA_merge.root
-path_output_file=../output/Beamline/auto/
-read -p "Make new histogram in $path_merge_file ? [Y / N] " -n 1 isNewFile
-echo -e "\n"
-if [ $isNewFile == "Y" ]
-then
-  echo Execute createHistogram
-  ./bin/Release/createHistogram $path_merge_file <<EOF
-  RECREATE
-  2
-  Initial_kinetic_energy_distribution
-  hist_initial_ke
-  100
-  100e-9
-  300e-9
-  Final_kinetic_energy_distribution
-  hist_final_ke
-  100
-  1e-11
-  130e-9
-EOF
-fi
-echo Execute addHistogram for initial kinetic energy
-for file in `ls $path_output_file`;
-do
-  ./bin/Release/addHistogram $path_output_file$file $path_merge_file <<EOF
-  hist_initial_ke
-  component_track_world_DATA
-  initial_kinetic_energy
-EOF
-done
-echo Execute addHistogram for final kinetic energy
-for file in `ls $path_output_file`;
-do
-  ./bin/Release/addHistogram $path_output_file$file $path_merge_file <<EOF
-  hist_final_ke
-  component_track_world_DATA
-  final_kinetic_energy
-EOF
-done
+
+cd ../output/Beamline/auto_parallel/
+hadd -f ../merge/beamline_surface_TRIGA_merge_parallel.root `ls`
